aef1104b9fddbf970463fcf00fbf11541158fac8
Igorix111 <igorapanasenko8@gmail.com>
2026-03-14 12:58:47 +0200

    Update API_KEY to use placeholder
    Replaced API key with a placeholder value.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿const API_KEY = "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"; // Вставьте ключ Groq сюда
+﻿const API_KEY = "const API_KEY = "PLACEHOLDER";"; // Вставьте ключ Groq сюда
 const GOOGLE_CLIENT_ID = "489570663546-fr6r6vccuu2nrl4k8f88m2lncm6c60cv.apps.googleusercontent.com"; // Вставьте Google Client ID сюда
 const ASSISTANT_NAME = "Fley";
 
